f41ca5cac5242931c872db72270127d6afe54375
mubeenabanu <mubeenabanu6125@gmail.com>
2022-10-21 22:26:33 +0530

    branch commit

---
+++ SeleniumFrameworkDesign/src/test/java/branchfile.java
@@ -0,0 +1,12 @@
+
+public class branchfile {
+	public void Method1()
+	{
+		System.out.println("this is new file");
+	}
+	public void Hi()
+	{
+		System.out.println("hi i am here a new branch file");
+	}
+
+}
--- SeleniumFrameworkDesign/src/test/java/newfile.java
+++ SeleniumFrameworkDesign/src/test/java/newfile.java
@@ -4,7 +4,12 @@ public class newfile {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("print my name -mubeena");
+		System.out.println("this is to create a branch");
 
+	}
+	public void Branch()
+	{
+		System.out.println("this is brnch");
 	}
 
 }
